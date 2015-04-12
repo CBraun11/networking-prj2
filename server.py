dd6020af942ae9f05d0b2ffafd35c7d986899bdb
senlin13gt <senlin13gt@gmail.com>
2015-04-12 13:59:00 -0400

    conflict fix

--- server.py
+++ server.py
@@ -6,26 +6,28 @@ state = sharedFunc.States.CLOSED
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
-def setupServer():
-	if state == sharedFunc.States.CLOSED:
-		sock = socket.socket(socket.AF_INET, # Internet
-		                     socket.SOCK_DGRAM) # UDP
-		sock.bind((UDP_IP, UDP_PORT))
-		sock.settimeout(2)
+if state == sharedFunc.States.CLOSED:
+	sock = socket.socket(socket.AF_INET, # Internet
+	                     socket.SOCK_DGRAM) # UDP
+	sock.bind((UDP_IP, UDP_PORT))
+	# sock.settimeout(2)
+	print "ready to listen"
 	state = sharedFunc.States.LISTEN
-	#ready to listen to clients
-	while state == sharedFunc.States.LISTEN:
-    		data, addr = sock.recvfrom(1024)
-    		## Need to check SYN and checksum
-    		if "SYN" in data:
-    			sock.sendto("SYN-ACK", (UDP_IP, UDP_PORT))
-    			state = sharedFunc.States.SYN_RCVD
-   	data, addr = sock.recvfrom(1024) 
-   	if (data == "ACK"):
-   		state = haredFunc.States.ESTABLISHED
-   		print "ESTABLISHED:"
+#ready to listen to clients
+while state == sharedFunc.States.LISTEN:
+	data, addr = sock.recvfrom(1024)
+	## Need to check SYN and checksum
+	if "SYN" in data:
+		print "sending syn-ack"
+		sock.sendto('SYN-ACK', (UDP_IP, UDP_PORT))
+		state = sharedFunc.States.SYN_RCVD
+	data, addr = sock.recvfrom(1024) 
+	while state != sharedFunc.States.ESTABLISHED:
+		if (data == "ACK"):
+			state = haredFunc.States.ESTABLISHED
+			print "ESTABLISHED:"
 
-while state == haredFunc.States.ESTABLISHED:
+while state == sharedFunc.States.ESTABLISHED:
 	print "running"
 	data, addr = sock.recvfrom(1024)
 
